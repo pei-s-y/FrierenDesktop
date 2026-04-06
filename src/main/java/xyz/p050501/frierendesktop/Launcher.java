275b4fdd9b3f7875ea5d8144a43ad1b1c9c9e23b
Frieren <psy20050501@outlook.com>
2026-04-07 01:09:19 +0800

    初步实现功能

---
+++ src/main/java/xyz/p050501/frierendesktop/Launcher.java
@@ -0,0 +1,9 @@
+package xyz.p050501.frierendesktop;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(FrierenApp.class, args);
+    }
+}
